73f8b29ed03d432832ce3bca386930764c0d831d
Reda <Redaelemawy98@gmail.com>
2022-03-13 16:48:19 +0200

    adding router dom

--- client/src/components/faculties/Engineering/Engineering.js
+++ client/src/components/faculties/Engineering/Engineering.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
-import Header from '../../Home_Page/Header'
-import Slider from '../../Home_Page/Slider'
-import NavFaculity from './NavFaculity'
-import cc from '../Images/2b305a0d-2116-4f8e-bd04-fb84321c235e (1).png'
+import React, { Component } from "react";
+import Header from "../../Home_Page/Header";
+import Slider from "../../Home_Page/Slider";
+import NavFaculity from "./NavFaculity";
+import cc from "../Images/2b305a0d-2116-4f8e-bd04-fb84321c235e (1).png";
 
 export default class Engineering extends Component {
-  
   render() {
-    let Engineering={
-      img:{cc},
-      Description:"Alaa"
-    }
+    let Engineering = {
+      img: { cc },
+      Description: "Alaa",
+    };
     return (
       <>
-     {/*<Header/>*/}
-     <NavFaculity/>
-     <Slider img={Engineering.img} Description={Engineering.Description}/>
+        {/*<Header/>*/}
+        <NavFaculity />
+        <Slider img={Engineering.img} Description={Engineering.Description} />
       </>
-    )
+    );
   }
 }
-
